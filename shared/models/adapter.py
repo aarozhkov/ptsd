12815dd0298e1782979f7ab89c0ce78bef79bb88
Vladimir Dyakov <vladimir.dyakov@nordigy.ru>
2021-10-22 18:15:10 +0300

    reformat TestRequest(BaseModel)

--- shared/models/adapter.py
+++ shared/models/adapter.py
@@ -1,13 +1,15 @@
-from typing import Optional, Set
-from pydantic import BaseModel, constr
+from typing import Optional
+
+from pydantic import BaseModel
+
 
 class TestRequest(BaseModel):
-    version: str = '0.1'
-    url: str = 'http://127.0.0.1:8114'
-    ptd_address: str = 'http://127.0.0.1:4444'
-    phone_or_email: str = '1234567890'
+    version: str = "0.1"
+    url: str = "http://127.0.0.1:8114"
+    ptd_address: str = "http://127.0.0.1:4444"
+    phone_or_email: str = "1234567890"
     extension: Optional[str]
     password: str
-    test_suite: str = 'video'
+    test_suite: str = "video"
     notify_on_complete: Optional[bool]
     test_id: int
