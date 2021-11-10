78b0b7002c2c8d82d3f1d2380aef24f9b8e7a0d6
Vladimir Dyakov <vladimir.dyakov@nordigy.ru>
2021-11-10 09:50:33 +0300

    remove defaults

--- shared/models/adapter.py
+++ shared/models/adapter.py
@@ -4,12 +4,12 @@ from pydantic import BaseModel
 
 
 class TestRequest(BaseModel):
-    version: str = "0.1"
-    url: str = "http://127.0.0.1:8114"
-    ptd_address: str = "http://127.0.0.1:4444"
-    phone_or_email: str = "1234567890"
+    version: str
+    url: str
+    ptd_address: str
+    phone_or_email: str
     extension: Optional[str]
     password: str
-    test_suite: str = "video"
+    test_suite: str
     notify_on_complete: Optional[bool]
     test_id: int
