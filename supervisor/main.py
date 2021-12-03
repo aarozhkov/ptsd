f986680e0b6587e1d3be80f56b8d428a4e466b21
Andrey Mashchenko <andrey.mashchenko@nordigy.ru>
2021-12-03 15:57:15 +0000

    [RCV-53030] Add helm charts

--- supervisor/main.py
+++ supervisor/main.py
@@ -8,5 +8,5 @@ supervisor_api = SupervisorApi(app_name="supervisor")
 
 
 if __name__=='__main__':
-    uvicorn.run(supervisor_api, host="0.0.0.0", port=8113)
+    uvicorn.run(supervisor_api, host="0.0.0.0", port=8000)
 
